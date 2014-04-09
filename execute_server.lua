341d7fc96ec1a30898e12ecb98bf780ae2e4d225
Paco Zamora Martinez <pakozm@gmail.com>
2014-04-09 22:29:01 +0200

    Added server execute script

--- execute_server.lua
+++ execute_server.lua
@@ -6,6 +6,7 @@ local reducefn = table.remove(arg, 1)
 local finalfn  = table.remove(arg, 1)
 --
 local server = require "mapreduce.server"
+local util = require "mapreduce.util"
 local s = server.new(connection_string, dbname)
 s:configure{
   taskfn      = taskfn,
@@ -17,4 +18,5 @@ s:configure{
   reduce_args = arg,
   final_args  = arg,
 }
+util.sleep(10)
 s:loop()
