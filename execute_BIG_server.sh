a570c9dd061aa4d2ba993fff4eb6c5be8a3210ae
Paco Zamora Martinez <pakozm@gmail.com>
2014-05-03 13:07:07 +0200

    Modified task examples

---
+++ execute_BIG_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LUA_PATH="?.lua"
+lua execute_server.lua django wordcountBIG \
+    examples.WordCountBig.taskfn \
+    examples.WordCount.mapfn \
+    examples.WordCount.partitionfn \
+    examples.WordCount.reducefn \
+    examples.WordCount.finalfn |
+sort -n > output
