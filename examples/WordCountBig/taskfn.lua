4e2c107de6143e2f9b8bf380097bff9d3f8e0e43
Paco Zamora Martinez <pakozm@gmail.com>
2014-05-03 13:25:20 +0200

    Minor change

--- examples/WordCountBig/taskfn.lua
+++ examples/WordCountBig/taskfn.lua
@@ -1,7 +1,6 @@
 return {
   -- init is for configuration purposes, it is allowed in any of the scripts
   init = function(arg)
-    for i,v in pairs(arg) do print(i,v) end
   end,
   func = function()
     local f = io.popen("ls /home/experimentos/CORPORA/EUROPARL/en-splits/*","r")
